af53b7861ae9f8054169ca5fc5a3869547962120
Daniela Bordeianu <danabordeianu55@gmail.com>
2025-10-12 10:51:22 -0400

    Configured "not an account error" in MainActivity.java and deleted HomepageActivity.java because it was redundant

--- app/src/main/java/com/example/logintest/domain/MainActivity.java
+++ app/src/main/java/com/example/logintest/domain/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.logintest.domain;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    //Database services
+    DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    //UI elements
+    EditText username;
+    EditText password;
+    Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Initialize  both firebase services
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        //Link UI elements for login
+        username = findViewById(R.id.user_name);
+        password = findViewById(R.id.user_password);
+        loginButton = findViewById(R.id.loginButton);
+        //When user clicks login button
+        loginButton.setOnClickListener (view -> {
+            validateAndLoginUser();
         });
     }
 
@@ -54,8 +82,45 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goToHome(View view){
-        Intent intent = new Intent(this, HomepageActivity.class);
-        startActivity(intent);
+
+    void validateAndLoginUser() {
+        //Get what the user typed and removed extra spaces
+        String usernameInput = username.getText().toString().trim();
+        String passwordInput = password.getText().toString().trim();
+
+        boolean everythingOK = true;
+
+        //Check username
+        if (usernameInput.isEmpty()) {
+            username.setError("Username (email) is required");
+            username.requestFocus();
+            return;
+        }
+        if (!usernameInput.contains("@") || !usernameInput.contains(".")) {
+            username.setError("Please enter a valid email address");
+            return;
+        }
+
+        //Check password
+        if (passwordInput.isEmpty()) {
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        //Verify if the user exists
+        mAuth.signInWithEmailAndPassword(usernameInput, passwordInput).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d("Firebase authentication", "Login successful for:" + username);
+                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                goToDashboard(null);
+            } else {
+                Log.w("Firebase authentification", "Login failed", task.getException());
+                Toast.makeText(MainActivity.this, "Not an account, please register. If you already have an account, try again.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
 }
